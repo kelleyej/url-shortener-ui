61458c8684d1b23bcca853159a7d29f75a87781e
Erin Kelley <erin.kelley01@gmail.com>
2024-04-22 09:38:21 -0600

    Add fetch functionality to grab url

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -8,17 +8,18 @@ function App () {
   const [urls, setUrls] = useState([]);
 
   useEffect(() => {
-
-  })
+    getUrls()
+    .then(data => setUrls(data.urls))
+  }, [])
 
   return (
     <main className="App">
       <header>
         <h1>URL Shortener</h1>
-        <UrlForm />
+        {/* <UrlForm /> */}
       </header>
 
-      <UrlContainer urls={"<<<Urls should go here>>>"}/>
+      <UrlContainer urls={urls}/>
     </main>
   );
 }
